a3d4eb37339eeae7cb2b54b491905177479dce25
Diyorbek <diyorbekjumanov1120@gmail.com>
2021-03-09 09:20:42 +0500

    create a even_sum_digit function

--- sum_digit.py
+++ sum_digit.py
@@ -2,7 +2,14 @@ def odd_sum_digit(l:list)->int:
     odd = 0
     for i in l:
         if i%2 == 1:
-            even += i
-    return even
+            odd += i
+    return odd
 
 # odd function
+
+def even_sum_digit(l1:list)->int:
+    even = 0
+    for x in l1:
+        if x%2 == 0:
+            even += x
+    return even
